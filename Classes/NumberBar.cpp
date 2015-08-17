abd1d9cd0604d77ca0f0e2b632028e0f06afa684
kerlw <kerl.d.w@gmail.com>
2015-08-17 15:09:58 +0800

    I've made a shelf background by myself, and now, the animal toy would be placed on the shelf :P

--- Classes/NumberBar.cpp
+++ Classes/NumberBar.cpp
@@ -8,7 +8,11 @@
 #include "NumberBar.h"
 #include "Const.h"
 
+#include "extensions/cocos-ext.h"
+
 USING_NS_CC;
+
+USING_NS_CC_EXT;
 
 const float NumberBar::SPRITE_GAP = 1.0f;
 
@@ -35,8 +39,8 @@ bool NumberBar::init() {
 		return false;
 
 	this->ignoreAnchorPointForPosition(false);
-	this->setBackGroundColor(Color3B(0,255,0));
-	this->setBackGroundColorType(ui::Layout::BackGroundColorType::SOLID);
+//	this->setBackGroundColor(Color3B(0,255,0));
+//	this->setBackGroundColorType(ui::Layout::BackGroundColorType::SOLID);
 	this->setAnchorPoint(Vec2(0.5, 0));
 
 	return true;
@@ -45,26 +49,40 @@ bool NumberBar::init() {
 void NumberBar::attachSprites(int number, SpriteFrameCache* cache) {
 	m_rectSprites.clear();
 
-	float x_offset = SPRITE_GAP;
-	float width = SPRITE_GAP, height = 0;
+	float x_offset = CELL_SIZE/2;
+	float width = 0, height = CELL_SIZE;
 	for (int index = 0; index < number; index++) {
 		auto sprite = Sprite::createWithSpriteFrame(cache->getSpriteFrameByName(FRAME_NAME[index]));
 		Rect rt = sprite->getTextureRect();
-		sprite->setPosition(Vec2(x_offset + rt.size.width/2, rt.size.height/2));
+		sprite->setPosition(Vec2(x_offset + rt.size.width/2, height - rt.size.height/2));
 
 		Rect rtSprite(x_offset, 0, rt.size.width, rt.size.height);
 		m_rectSprites.push_back(rtSprite);
 
 //		log("rect of sprite %d is %f,%f,%f,%f", index, rtSprite.origin.x, rtSprite.origin.y, rtSprite.size.width, rtSprite.size.height);
 
-		height = (rt.size.height > height ? rt.size.height : height);
+//		height = (rt.size.height > height ? rt.size.height : height);
 		width += rt.size.width + SPRITE_GAP;
 		this->addChild(sprite);
 
 		x_offset += rt.size.width + SPRITE_GAP;
 	}
-	height += 2 * SPRITE_GAP;
+//	height += 2 * SPRITE_GAP;
+	width += CELL_SIZE;
 	this->setContentSize(Size(width, height));
+
+	//add background
+	auto frm = cache->getSpriteFrameByName(FRAME_NAME[SHELF_INDEX]);
+	if (frm) {
+		auto bkg = Scale9Sprite::createWithSpriteFrame(frm);
+		bkg->setPreferredSize(Size(width, height));
+		bkg->setCapInsets(Rect(70, 70, 1, 1));
+		bkg->setPosition(Vec2(width/2, height/2));
+		this->addChild(bkg, -1);
+	} else {
+		this->setBackGroundColor(Color3B(0,255,0));
+		this->setBackGroundColorType(ui::Layout::BackGroundColorType::SOLID);
+	}
 //	log("this content size is %f, %f", width, height);
 }
 
